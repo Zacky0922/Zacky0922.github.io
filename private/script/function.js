5f56de4ad5757b3a1bb641c1f9d3d4252af2bd10
Zacky <abcn00200@yahoo.co.jp>
2020-05-28 23:07:45 +0900

    fix

--- private/script/function.js
+++ private/script/function.js
@@ -257,7 +257,7 @@ function reMathJax(id = null) {
 
 //service workerの登録関係
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register(getParam["lv"] + 'service_worker.js').then(function (registration) {
+    navigator.serviceWorker.register('service_worker.js').then(function (registration) {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }).catch(function (err) {
         console.log('ServiceWorker registration failed: ', err);
---
+++ private/service_worker.js
@@ -0,0 +1,31 @@
+
+// キャッシュファイルの指定
+// オフライン利用可
+var CACHE_NAME = 'Z-PWA';
+var urlsToCache = [
+    'index.html'
+];
+
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
+
