e0334cacb60a0d17e6875458562468b4e5bd963f
Zacky <abcn00200@yahoo.co.jp>
2020-05-28 21:38:40 +0900

    add 模試タイマーv20200602

---
+++ PWApps/模試タイマーv20200602/service_worker.js
@@ -0,0 +1,29 @@
+
+// キャッシュファイルの指定
+var CACHE_NAME = '模試タイマーv20200602';
+var urlsToCache = [
+    './',
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response ? response : fetch(event.request);
+            })
+    );
+});
+
