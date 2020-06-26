ca199b0975ea60ec4d64c4967e8c0ae5908fa453
Zacky <abcn00200@yahoo.co.jp>
2020-06-26 10:46:49 +0900

    add quickEditor

--- scripts/extTools/prittyprint/prittyprint.js
+++ scripts/extTools/prittyprint/prittyprint.js
@@ -10,32 +10,65 @@ function setQuickEditor(id,tx = "") {
     ele.classList.add("prettyprintQuickEditor");
 
     var defaultSource = [
-        '<!-- ここの内容をHTMLにコピペしましょう -->\n' +
-        '<html>\n<head>\n' +
-        '\t<title>タイトル</title>' +
-        '\n</html>\n<body>\n',
-        '</body>\n</html>'
+        ['<!-- ここの内容をHTMLにコピペしましょう -->',
+            '<html>',
+            '<head>',
+            '<title>タイトル</title>',
+            '</head>',
+            '<body>'],
+        ['</body>',
+            '</html>']
     ];
 
+
     var txarea = document.createElement("textarea");
-    txarea.setAttribute("onclick",
+    txarea.onkeydown = function (e) { zEnableTab(e, this); }
+
+    txarea.setAttribute("onkeyup",
         "this.parentElement.children[1].innerText='" +
-        defaultSource[0] + txarea.value + defaultSource[1] + "';" +
-        "rePrettyprint(this.parentNode.children[1].id);");
-
-
+        defaultSource[0].join("\\n") + "\\n'+this.value+'\\n" + defaultSource[1].join("\\n") + "';" +
+        "rePrettyprint(this.parentElement.children[1].id);" +
+        "this.parentElement.children[2].innerHTML=this.value");
+    
     var pre = document.createElement("pre");
-    pre.id = "prettyprint_" + Math.floor(Math.random() * 10);
-    pre.classList.add("prettyprint");
-    pre.classList.add("linenums");
-    pre.innerText = defaultSource[0] + txarea.value + defaultSource[1];
+    pre.id = "prettyprintQuickPreviewer_" + Math.floor(Math.random() * 1024);
+    pre.classList.add("prettyprint","linenums");
+    pre.innerText = defaultSource[0].join("\n") +
+        txarea.value + "\n" +
+        defaultSource[1].join("\n");
 
     var preview = document.createElement("div");
-    preview.innerHTML = pre.innerText;
+    preview.classList.add("prettyprintQuickPreviewer");
+    //preview.innerHTML = pre.innerText;
 
     ele.appendChild(txarea);
     ele.appendChild(pre);
     ele.appendChild(preview);
 
     rePrettyprint(id);
+}
+
+//タブ入力を有効にする（タブでの移動をキャンセルする）
+//http://www.webclap-dandy.com/?category=Programing&id=5
+function zEnableTab(e, obj) {
+    //タブキー以外は無視
+    if (e.keyCode != 9) {
+        return;
+    }
+
+    //タブキーの標準挙動（次へ移動）をキャンセル
+    e.preventDefault();
+
+    // 現在のカーソルの位置と、カーソルの左右の文字列を取得しておく
+    var cursorPosition = obj.selectionStart;
+    var cursorLeft = obj.value.substr(0, cursorPosition);
+    var cursorRight = obj.value.substr(cursorPosition, obj.value.length);
+
+    // テキストエリアの中身を、
+    // 「取得しておいたカーソルの左側」+「タブ」+「取得しておいたカーソルの右側」
+    // という状態にする。
+    obj.value = cursorLeft + "\t" + cursorRight;
+
+    // カーソルの位置を入力したタブの後ろにする
+    obj.selectionEnd = cursorPosition + 1;
 }
