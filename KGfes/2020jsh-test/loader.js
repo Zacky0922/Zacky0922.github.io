4523d2a3ddf1c85e2c21ddd8f622283f6ddafbbd
Zacky <abcn00200@yahoo.co.jp>
2020-08-26 19:52:57 +0900

    fix

--- KGfes/2020jsh-test/loader.js
+++ KGfes/2020jsh-test/loader.js
@@ -15,10 +15,20 @@
 	}
 	script.src = basicUrl + "zScripts/master.js?mode=kgfes";
 	document.write(script.outerHTML);
-	
+
 
 	var manualQuery = new Array();
 	manualQuery["mode"] = "kgfes_debug";
 	manualQuery["root"] = basicUrl;
 	manualQuery["zScriptsDir"] = basicUrl + "zScripts/";
 })();
+
+
+let ua = window.navigator.userAgent.toLowerCase();
+
+if (ua.indexOf("edg") > -1) {
+	if (ua.indexOf("edge") > -1) {
+		alert("お使いのブラウザは「古い」Edgeです。");
+		location.href = "./br_error.html";
+	}
+}
