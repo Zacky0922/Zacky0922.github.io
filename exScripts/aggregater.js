df5515c016411764bce6721742b99b813dbb8235
Zacky <abcn00200@yahoo.co.jp>
2020-09-01 22:54:55 +0900

    fix

--- exScripts/aggregater.js
+++ exScripts/aggregater.js
@@ -3,25 +3,15 @@
 // ex
 import 'https://code.jquery.com/jquery-3.3.1.min.js'; //これでデータはとれるが…
 import { getGicon } from "./ex/MaterialIcons/googleicon.js";    // func
+// tools
+import { zDate } from "./tool/date.js";    // class
+import { zList, burgerMenu } from "./tool/list.js";    // class
+
 
 // tools
-//ダメ
-//import { zDate } from "./tool/date.js";    // class
-
-//ダメ
-//import { zList } from "./tool/list.js";    // class
-
-// 処理の都合上、最後がいいかも？
-import { zExportTest } from "./tool/test.js";    // let
-//zExportTest();
-import { zExportTest as zExportTest2 ,a} from "./test.js";    // let
-//zExportTest2();
-let aa = new a();
-
-//だめ
-//import { zDebug } from "./tool/debug/debug.js";    // let
+import { zDebug } from "./tool/debug/debug.js";    // let
 
 
-export { getGicon,  zExportTest };
 
-alert("I AM AGGRETAER! - 28");
+
+export { getGicon, zDate, zList, burgerMenu,zDebug};
