442d2061a8026195159720ec3b6964f43f8d7aa1
Zacky <abcn00200@yahoo.co.jp>
2020-09-07 01:01:50 +0900

    lab/index module
    ES moduleとbootstrapの導入
    importer未設定（相対がブレないから、いらないかもだけど・・・）

--- exScripts/aggregater.js
+++ exScripts/aggregater.js
@@ -1,17 +1,35 @@
 // Module Aggregater
 
+/*  MathJaxは公式でES module対応中？
+    現状は、通常の読込が必要。
+    aggregaterより先に以下を記述
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_CHTML"></script>
+
+    "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS_CHTML", //old
+    "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js",                        //new
+
+*/
+
 // ex
-import 'https://code.jquery.com/jquery-3.3.1.min.js'; //これでデータはとれるが…
+//import 'https://code.jquery.com/jquery-3.3.1.min.js';
+import "./ex/jQuery/jquery-3.5.1.min.js";
+import "./ex/jQuery/jquery-ui.min.js";
+import { zTab } from "./ex/jQuery/jquery_Zacky.js";
 import { getGicon } from "./ex/MaterialIcons/googleicon.js";    // func
+
 
 // tools
 import { zDate } from "./tool/date.js";    // class
 import { zList, burgerMenu } from "./tool/list.js";    // class,class
+import { zMathJax } from "./tool/math/MathJaxMacro.js";
+import { zMath } from "./tool/math/zMath.js";
 
-// tools
+// これは最後に読み込む
 import { zDebug } from "./tool/debug/debug.js";    // let
 
-
+// まとめ
+export {
+    zTab, getGicon,
+    zDate, zList, burgerMenu, zMathJax, zMath, zDebug
+};
 zDebug.addLog("Module Aggregater loaded!");
-
-export { getGicon, zDate, zList, burgerMenu, zDebug };
---
+++ exScripts/tool/math/MathJaxMacro.js
@@ -0,0 +1,123 @@
+
+
+//	===	MathJax マクロ設定 ===
+//window.MathJax = {
+MathJax.Hub.Config({
+    extensions: [	//拡張機能			個別読込($$～$$内記述)		説明		
+        //	'AMScd.js'		//	\(\require{AMScd}\)  		//可換図式	
+    ],
+    tex2jax: {
+        inlineMath: [
+            ['$', '$'],
+            ["\\(", "\\)"],
+            ["`", "`"]      //draw.ioプレビュー用
+        ],
+        displayMath: [
+            ['$$', '$$'],
+            ["\\[", "\\]"]
+        ],
+        processEscapes: true    //欄外で$を使用可にする
+    },
+    TeX: {
+        Macros: {
+            //\displaystyle簡略表記
+            d: '\\displaystyle ',
+
+            //定義⇔{def}
+            def: '{\\overset{def}{\\iff}}',
+
+            //\color簡略表記
+            black: ['\\color{black}{ #1 }', 1],
+            red: ['\\color{red}{ #1 }', 1],
+            blue: ['\\color{blue}{ #1 }', 1],
+            green: ['\\color{green}{ #1 }', 1],
+
+            //such that記号
+            st: '{\\quad\\text{s.t.}\\quad}',
+
+            //増分記号
+            diff: '{\\mathit{\\Delta}}',
+            //微分記号
+            dydx: '{\\dfrac{dy}{dx}}',
+            ddx: '{\\dfrac{d}{dx}}',
+            ddt: '{\\dfrac{d}{dt}}',
+            dd: ['{\\dfrac{d}{d #1}}', 1],
+            //増減凹凸表の矢印
+
+            //ベクトル記号
+            vec: ['{\\overrightarrow{ \\mathstrut \\it{#1} } }', 1],
+            //大文字立体ベクトル記号
+            Vec: ['{\\overrightarrow{ \\mathstrut \\rm{#1}}}', 1],
+
+            //行列
+            tr: '{ \\rm tr }',
+
+            //BlackBoardBold
+            bbb: ['{ {\\mathbb \\it #1 } }', 1],
+
+            //列ベクトル成分表記
+            cvec: ['{ \\left\( \\mathstrut \\begin\{array\} \{c\} {#1} \\\\ {#2} \\\\ \\end\{array\} \\right\) }', 2],
+            ccvec: ['{ \\left\( \\mathstrut \\begin\{array\} \{c\} {#1} \\\\ {#2} \\\\ {#3} \\\\ \\end\{array\} \\right\) }', 3],
+            //ccvec：cubic col vector
+
+            //ノルム・絶対値
+            norm: ['{\\left| \\mathstrut #1 \\right|}', 1],
+
+            //区切り縦棒(群数列など)：section barの略語のつもり
+            secBar: '{\\ \\LARGE{|} \\ }',
+
+            //平行記号
+            parallel: '\\verb+\/+\\verb+\/+',	//∥∦
+
+            //かっこ
+            bracket: ['\\left\( \\mathstrut #1 \\right\)', 1],
+            Bracket: ['\\left\\{ \\mathstrut #1 \\right\\}', 1],
+            BRACKET: ['\\left\[ \\mathstrut #1 \\right\]', 1],
+            abs: ['\\left\| \\mathstrut #1 \\right\|', 1],
+            //場合の数
+            permutation: ['_{#1} {\\rm P}_{#2}', 2],
+            combination: ['_{#1} {\\rm C}_{#2}', 2],
+
+            /* trial macro */
+
+        }
+    }
+});
+//};
+
+//読込完了をここで宣言するのが約束らしい
+//MathJax.Ajax.loadComplete("https://zacky0922.github.io/scripts/extTools/MathJaxMacro.js");
+
+
+
+
+class zMathJax{
+    
+
+
+    static reMathJax(id = null) {
+        if (id == null) {
+            //全体を再描画
+            
+        } else {
+            MathJax.Hub.Queue(['Typeset', MathJax.Hub, id]);
+            // MathJax.Hub.Queue(['Typeset', MathJax.Hub, id], [func]);
+            // func＝終了後に実行したいメソッド名
+        }
+    }
+
+    // MathJaxテンプレート集
+    static templete(keyword) {
+        switch (keyword) {
+            case "オイラーの公式":
+                return "e^{i\\theta}=\\cos\\theta+i\\sin\\theta";
+            case "三平方の定理":
+            case "ピタゴラス定理":
+                return "a^2+b^2=c^2";
+            default:
+                return "Error - No Keyword.";
+        }
+    }
+}
+
+export { zMathJax };
