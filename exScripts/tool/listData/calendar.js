71ce3bd97d1a10b2885d9629adcd17c62375ae6e
Zacky <abcn00200@yahoo.co.jp>
2020-09-17 18:31:20 +0900

    fix

--- exScripts/tool/listData/calendar.js
+++ exScripts/tool/listData/calendar.js
@@ -1,17 +1,19 @@
 // 非export変数
-// 本日
-let today = new Date();
+let today = new Date();     // 本日
 // 予定登録
 // 複数のカレンダーを同一ページに表示する可能性を考えて次の連想配列を適用
-// array[this.id][i] = [y/0,m,d/0,week,date,html,divCl,tdCl]
-// y=0 毎年繰り返し　d=0 第n週○曜日指定
+// array[this.id][i] = [y/0,m/0,d,week/0,date,html,divCl,tdCl]
+// y,m,w=0 毎年・毎月・毎週 繰り返し
 let schedule = new Array();
-let setRegularSchedule = new Array();   // 毎年イベント登録済みフラグ
+let setRegularSchedule = new Array();   // 反復イベント登録済みフラグ（西暦）
 
 class zCalendar {
     constructor(id) {
         this.id = id;
         this.wrap = document.getElementById(id);
+        // 予定一覧init
+        schedule[this.id] = new Array();
+        setRegularSchedule[this.id] = new Array();
 
         // table初期化
         this.tbody = document.createElement("tbody");   // captionイベントハンドラ用に早めに生成
@@ -27,16 +29,14 @@ class zCalendar {
         this.tbl.appendChild(this.tbody);
     }
 
-
     // 祝祭日設定
-    setHolidays(csv, y = this.y) {
+    setHolidays(csv, y = (new Date()).getFullYear()) {
         // 重複セット防止
-        if (this.setHolidayYear.includes(y)) {
+        if (setRegularSchedule[this.id].includes(y)) {
             return;
         } else {
-            this.setHolidayYear.push(y);
+            setRegularSchedule[this.id].push(y);
         }
-        return;
 
         // 祝祭日セット
         for (let i = 1; i < csv.length; i++) {
@@ -48,6 +48,8 @@ class zCalendar {
             if (csv[i][6] != 0 && csv[i][6] < y) {
                 continue;
             }
+            this.addSchedule(y, csv[i][0], csv[i][1], csv[i][2], csv[i][3], csv[i][4], null, "holiday");
+            continue;
 
             if (csv[i][1] != 0) {
                 // 固定祝祭日
@@ -60,6 +62,9 @@ class zCalendar {
                 this.addSchedule(y, csv[i][0], d, csv[i][4], null, "holiday");
             }
         }
+
+        return;
+
         // 自動計算　参考　https://enterprisezine.jp/iti/detail/865
         // 春分の日
         this.addSchedule(y, 3,
@@ -84,10 +89,10 @@ class zCalendar {
     }
 
     // 予定追加
-    addSchedule(y, m, d, html, divCl = null, tdCl = null) {
-        this.schedule[this.schedule.length] = [y, m, d, html, divCl, tdCl];
+    addSchedule(y, m, d, week, date, html, divCl = null, tdCl = null) {
+        // array[this.id][i] = [y/0, m/0, d, week/0, date, html, divCl, tdCl]
+        schedule[this.id][schedule.length] = [y, m, d, week, date, html, divCl, tdCl];
     }
-
 
     setCaption(y, m) {
         // caption生成
@@ -98,6 +103,9 @@ class zCalendar {
         let preM = document.createElement("span");
         preM.classList.add("zCal_control");
         preM.innerText = "[前月]";
+        let now = document.createElement("span");
+        now.classList.add("zCal_control");
+        now.innerText = "[今月]";
         let ym = document.createElement("span");
         ym.innerText = y + "-" + m;   // 以下のイベント処理に影響するのでtext format確認
         let proM = document.createElement("span");
@@ -108,6 +116,7 @@ class zCalendar {
         proY.innerText = "[次年]";
         cap.appendChild(preY);
         cap.appendChild(preM);
+        cap.appendChild(now);
         cap.appendChild(ym);
         cap.appendChild(proM);
         cap.appendChild(proY);
@@ -130,6 +139,11 @@ class zCalendar {
             } else {
                 ymd[1] = Number(ymd[1]) - 1;
             }
+            ym.innerText = ymd[0] + "-" + ymd[1];
+            f(ymd[0], ymd[1], tbody);
+        });
+        now.addEventListener("click", function () {
+            let ymd = [today.getFullYear(),today.getMonth()+1];
             ym.innerText = ymd[0] + "-" + ymd[1];
             f(ymd[0], ymd[1], tbody);
         });
@@ -171,19 +185,16 @@ class zCalendar {
         this.tbl.appendChild(thead);
     }
 
-    // イベントハンドラから呼ばれる可能性があるので「this」禁止
-    setCalendar(y, m, tbody) {
+    // イベントハンドラから呼ばれる可能性があるので「this」禁止（＝対策としてtbodyを引数で利用）
+    setCalendar(y = today.getFullYear(), m = today.getMonth() + 1, tbody) {
+        // init
         tbody.innerHTML = "";
-        if (y == undefined) {
-            y = today.getFullYear();
-        }
-        if (m == undefined) {
-            m = today.getMonth() + 1;
-        }
+
         // 月の初日の曜日を取得
         let blankCells = (new Date(y, m - 1, 1)).getDay();   // 日＝0,土=6
         // 日付
         let d = 1 - blankCells;
+        let endDay = (new Date(y, m, 0)).getDate(); // 翌月1日
         while (true) {
             let tr = document.createElement("tr");
             tbody.appendChild(tr);
@@ -206,12 +217,12 @@ class zCalendar {
                 tr.appendChild(td);
 
                 // 予定があれば追加
-                /*
                 for (let s = 0; s < schedule.length; s++) {
+                    // 固定日登録
                     if (
-                        (schedule[s][0] == y) &&
-                        (schedule[s][1] == m) &&
-                        (schedule[s][2] == d)
+                        (schedule[s][0] != 0) &&
+                        (schedule[s][1] != 0) &&
+                        (schedule[s][2] != 0)
                     ) {
                         let ele = document.createElement("div");
                         ele.innerHTML = schedule[s][3];
@@ -224,14 +235,13 @@ class zCalendar {
                         td.appendChild(ele);
                     }
                 }
-                */
 
                 d++;
-                if (d > (new Date(y, m, 0)).getDate()) {
+                if (d > endDay) {
                     break;  //最終日到達でbreak
                 }
             }
-            if (d > (new Date(y, m, 0)).getDate()) {
+            if (d > endDay) {
                 break;  //最終日到達でbreak
             }
         }
